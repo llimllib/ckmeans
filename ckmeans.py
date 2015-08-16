94e344b63246c6a5cf51c2e09e20e68ad6d3bb19
Bill Mill <bill@billmill.org>
2015-08-15 23:53:37 -0400

    do a bunch of renaming

--- ckmeans.py
+++ ckmeans.py
@@ -4,58 +4,56 @@ def ckmeans(data, n_clusters):
     if n_clusters > len(data):
         raise ValueError("Cannot generate more classes than there are data values")
 
+    # if there's only one value, return it; there's no sensible way to split
+    # it. This means that len(ckmeans([data], 2)) may not == 2. Is that OK?
+    if len(set(data)) == 1:
+        return [data]
+
+    data.sort()
     n = len(data)
-
-    # XXX: probably more efficient to leave this as an iterator, but let's get
-    # it correct first
-    sorted_data = list(sorted(data))
-    unique_count = len(set(sorted_data))
-
-    if unique_count == 1:
-        return [sorted_data]
 
     matrix = np.zeros((n_clusters, n))
     backtrack_matrix = np.zeros((n_clusters, n), dtype=np.int)
 
     for cluster in range(n_clusters):
-        first_cluster_mean = sorted_data[0]
+        first_cluster_mean = data[0]
 
-        for sorted_idx in range(max(cluster, 1), n):
+        for data_idx in range(max(cluster, 1), n):
             if cluster == 0:
-                squared_difference = (sorted_data[sorted_idx] - first_cluster_mean) ** 2
-                matrix[cluster][sorted_idx] = matrix[cluster][sorted_idx - 1] + ((sorted_idx - 1) / sorted_idx) * squared_difference
+                squared_difference = (data[data_idx] - first_cluster_mean) ** 2
+                matrix[cluster][data_idx] = matrix[cluster][data_idx - 1] + ((data_idx - 1) / data_idx) * squared_difference
 
-                new_sum = sorted_idx * first_cluster_mean + sorted_data[sorted_idx]
-                first_cluster_mean = new_sum / sorted_idx
+                new_sum = data_idx * first_cluster_mean + data[data_idx]
+                first_cluster_mean = new_sum / data_idx
             else:
                 sum_squared_distances = 0
                 mean_xj = 0
 
-                for j in range(sorted_idx, cluster-1, -1):
-                    sum_squared_distances += (sorted_idx - j) / (sorted_idx - j + 1) * (sorted_data[j] - mean_xj)**2
+                for j in range(data_idx, cluster-1, -1):
+                    sum_squared_distances += (data_idx - j) / (data_idx - j + 1) * (data[j] - mean_xj)**2
 
-                    mean_xj = (sorted_data[j] + ((sorted_idx - j) * mean_xj)) / (sorted_idx - j + 1)
+                    mean_xj = (data[j] + ((data_idx - j) * mean_xj)) / (data_idx - j + 1)
 
-                    if j == sorted_idx:
-                        matrix[cluster][sorted_idx] = sum_squared_distances
-                        backtrack_matrix[cluster][sorted_idx] = j
+                    if j == data_idx:
+                        matrix[cluster][data_idx] = sum_squared_distances
+                        backtrack_matrix[cluster][data_idx] = j
                         if j > 0:
-                            matrix[cluster][sorted_idx] += matrix[cluster - 1][j - 1]
+                            matrix[cluster][data_idx] += matrix[cluster - 1][j - 1]
                     else:
                         if j == 0:
-                            if sum_squared_distances <= matrix[cluster][sorted_idx]:
-                                matrix[cluster][sorted_idx] = sum_squared_distances
-                                backtrack_matrix[cluster][sorted_idx] = j
-                        elif sum_squared_distances + matrix[cluster-1][j-1] < matrix[cluster][sorted_idx]:
-                            matrix[cluster][sorted_idx] = sum_squared_distances + matrix[cluster-1][j-1]
-                            backtrack_matrix[cluster][sorted_idx] = j
+                            if sum_squared_distances <= matrix[cluster][data_idx]:
+                                matrix[cluster][data_idx] = sum_squared_distances
+                                backtrack_matrix[cluster][data_idx] = j
+                        elif sum_squared_distances + matrix[cluster-1][j-1] < matrix[cluster][data_idx]:
+                            matrix[cluster][data_idx] = sum_squared_distances + matrix[cluster-1][j-1]
+                            backtrack_matrix[cluster][data_idx] = j
 
     clusters = []
     cluster_right = len(backtrack_matrix[0]) - 1
 
     for cluster in range(len(backtrack_matrix)-1, -1, -1):
         cluster_left = backtrack_matrix[cluster][cluster_right]
-        clusters.insert(0, sorted_data[cluster_left:cluster_right+1])
+        clusters.insert(0, data[cluster_left:cluster_right+1])
 
         if cluster > 0:
             cluster_right = cluster_left - 1
