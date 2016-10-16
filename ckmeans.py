ae87a347b7d5d3632fca3becab54bda4cf848472
Bill Mill <bill@billmill.org>
2016-10-16 16:40:32 -0400

    Update ckmeans to version 3.4.6
    * Thanks to @schnerd for alerting me that there was an updated version

--- ckmeans.py
+++ ckmeans.py
@@ -1,67 +1,115 @@
 import numpy as np
+from operator import itemgetter
+
+def ssq(j, i, sum_x, sum_x_sq):
+    if (j > 0):
+        muji = (sum_x[i] - sum_x[j-1]) / (i - j + 1)
+        sji = sum_x_sq[i] - sum_x_sq[j-1] - (i - j + 1) * muji ** 2
+    else:
+        sji = sum_x_sq[i] - sum_x[i] ** 2 / (i+1)
+
+    return 0 if sji < 0 else sji
+
+def fill_row_k(imin, imax, k, S, J, sum_x, sum_x_sq, N):
+    if imin > imax: return
+
+    i = (imin+imax) // 2
+    S[k][i] = S[k-1][i-1]
+    J[k][i] = i
+
+    jlow = k
+
+    if imin > k:
+        jlow = int(max(jlow, J[k][imin-1]))
+    jlow = int(max(jlow, J[k-1][i]))
+
+    jhigh = i-1
+    if imax < N-1:
+        jhigh = int(min(jhigh, J[k][imax+1]))
+
+    for j in range(jhigh, jlow-1, -1):
+        sji = ssq(j, i, sum_x, sum_x_sq)
+
+        if sji + S[k-1][jlow-1] >= S[k][i]: break
+
+        # Examine the lower bound of the cluster border
+        # compute s(jlow, i)
+        sjlowi = ssq(jlow, i, sum_x, sum_x_sq)
+
+        SSQ_jlow = sjlowi + S[k-1][jlow-1]
+
+        if SSQ_jlow < S[k][i]:
+            S[k][i] = SSQ_jlow
+            J[k][i] = jlow
+
+        jlow += 1
+
+        SSQ_j = sji + S[k-1][j-1]
+        if SSQ_j < S[k][i]:
+            S[k][i] = SSQ_j
+            J[k][i] = j
+
+    fill_row_k(imin, i-1, k, S, J, sum_x, sum_x_sq, N)
+    fill_row_k(i+1, imax, k, S, J, sum_x, sum_x_sq, N)
+
+def fill_dp_matrix(data, S, J, K, N):
+    sum_x = np.zeros(N, dtype=np.float_)
+    sum_x_sq = np.zeros(N, dtype=np.float_)
+
+    # median. used to shift the values of x to improve numerical stability
+    shift = data[N//2]
+
+    for i in range(N):
+        if i == 0:
+            sum_x[0] = data[0] - shift
+            sum_x_sq[0] = (data[0] - shift) ** 2
+        else:
+            sum_x[i] = sum_x[i-1] + data[i] - shift
+            sum_x_sq[i] = sum_x_sq[i-1] + (data[i] - shift) ** 2
+
+        S[0][i] = ssq(0, i, sum_x, sum_x_sq)
+        J[0][i] = 0
+
+    for k in range(1, K):
+        if (k < K-1):
+            imin = max(1, k)
+        else:
+            imin = N-1
+
+        fill_row_k(imin, N-1, k, S, J, sum_x, sum_x_sq, N)
 
 def ckmeans(data, n_clusters):
+    if n_clusters <= 0:
+        raise ValueError("Cannot classify into 0 or less clusters")
     if n_clusters > len(data):
         raise ValueError("Cannot generate more classes than there are data values")
 
     # if there's only one value, return it; there's no sensible way to split
     # it. This means that len(ckmeans([data], 2)) may not == 2. Is that OK?
-    if len(set(data)) == 1:
+    unique = len(set(data))
+    if unique == 1:
         return [data]
 
     data.sort()
     n = len(data)
 
-    # The table we're filling up with withinss values
-    D = np.zeros((n_clusters, n))
+    S = np.zeros((n_clusters, n), dtype=np.float_)
 
-    # XXX: not clear on what goes in B?
-    B = np.zeros((n_clusters, n), dtype=np.int)
+    J = np.zeros((n_clusters, n), dtype=np.uint64)
 
-    for cluster in range(n_clusters):
-        first_cluster_mean = data[0]
-
-        for data_idx in range(max(cluster, 1), n):
-            if cluster == 0:
-                squared_difference = (data[data_idx] - first_cluster_mean) ** 2
-                D[cluster][data_idx] = D[cluster][data_idx - 1] + (data_idx / (data_idx + 1)) * squared_difference
-
-                new_sum = data_idx * first_cluster_mean + data[data_idx]
-                first_cluster_mean = new_sum / (data_idx + 1)
-            else:
-                sum_squared_distances = 0
-                mean_xj = 0
-
-                for j in range(data_idx, cluster-1, -1):
-                    sum_squared_distances += (data_idx - j) / (data_idx - j + 1) * (data[j] - mean_xj)**2
-
-                    mean_xj = (data[j] + ((data_idx - j) * mean_xj)) / (data_idx - j + 1)
-
-                    if j == data_idx:
-                        D[cluster][data_idx] = sum_squared_distances
-                        B[cluster][data_idx] = j
-                        if j > 0:
-                            D[cluster][data_idx] += D[cluster - 1][j - 1]
-                    else:
-                        if j == 0:
-                            if sum_squared_distances <= D[cluster][data_idx]:
-                                D[cluster][data_idx] = sum_squared_distances
-                                B[cluster][data_idx] = j
-                        elif sum_squared_distances + D[cluster-1][j-1] < D[cluster][data_idx]:
-                            D[cluster][data_idx] = sum_squared_distances + D[cluster-1][j-1]
-                            B[cluster][data_idx] = j
+    fill_dp_matrix(data, S, J, n_clusters, n)
 
     clusters = []
-    cluster_right = len(B[0]) - 1
+    cluster_right = n-1
 
-    for cluster in range(len(B)-1, -1, -1):
-        cluster_left = B[cluster][cluster_right]
-        clusters.insert(0, data[cluster_left:cluster_right+1])
+    for cluster in range(n_clusters-1, -1, -1):
+        cluster_left = int(J[cluster][cluster_right])
+        clusters.append(data[cluster_left:cluster_right+1])
 
         if cluster > 0:
             cluster_right = cluster_left - 1
 
-    return clusters
+    return list(reversed(clusters))
 
 ##
 ## HELPER CODE FOR TESTS
@@ -101,7 +149,6 @@ def min_squared_distance(data, n):
     return min((sum_of_squared_distances(partition), partition)
                 for partition in partition_n(data, n))
 
-
 if __name__ == "__main__":
     try:
         ckmeans([], 10)
@@ -128,12 +175,17 @@ if __name__ == "__main__":
 
     for test in tests:
         args, expected = test
-        result = ckmeans(*args)
-        errormsg = "ckmeans({}) = {} != {}\n{} > {}".format(
+        try:
+            result = ckmeans(*args)
+        except:
+            print("✗ {}, {}".format(args[0], args[1], result))
+            raise
+        errormsg = "✗ ckmeans({}) = {} != {}\n{} > {}".format(
                 args, result, expected,
                 sum_of_squared_distances(result),
                 sum_of_squared_distances(expected))
         assert np.array_equal(result, expected), errormsg
+        print("✓ {}".format(result))
 
     from hypothesis import given
     from hypothesis.strategies import lists, integers, just, tuples
@@ -141,7 +193,7 @@ if __name__ == "__main__":
 
     # can we set max higher? let's start with this number and see...
     for n in range(2,10):
-        @given(lists(integers(), min_size=n, max_size=20).filter(lambda lst: len(set(lst)) > 1))
+        @given(lists(integers(min_value=-100, max_value=100), min_size=n, max_size=20).filter(lambda lst: len(set(lst)) > 1))
         def test_ckmeans(data):
             result = ckmeans(data, n)
 
